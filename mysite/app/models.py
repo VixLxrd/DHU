5526def7e1d9ea905c82e5f6545b71411cc0a3ac
max <komuxin06@mail.ru>
2021-03-20 16:37:00 +0500

    added database view

--- mysite/app/models.py
+++ mysite/app/models.py
@@ -1,15 +1,8 @@
 from django.db import models
 
 
-class Area(models.Model):
-    region = models.CharField(max_length=200)
-
-
-class City(models.Model):
-    area = models.ForeignKey(Area, on_delete=models.CASCADE)
-
-
 class Home(models.Model):
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    region = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     levels = models.PositiveSmallIntegerField()
---
+++ mysite/create_db.py
@@ -0,0 +1,4 @@
+from app.models import Home
+
+
+a = Home(region=)
