2ae2b995d3510f9a79eb9ae45792572b22f39e80
VixLxrd <mr.hll.vixlxrd@gmail.com>
2021-03-20 20:06:35 +0500

    Копия
    Приложение с возможностью организации базы данных

---
+++ dhu_project/create_db.py
@@ -0,0 +1,8 @@
+# RUN FROM SHELL
+# 
+# from models import Home
+# import random as _
+
+# for mod in range(3):
+#     person = Home(city = _.randint(1, 5), adress = _.randint(1, 8), date = _.randint(1, 12), temp = _.randint(400, 900), aqua_cold = float(_.randint(1, 5) + _.randint(1, 10) // 10), aqua_hot = float(_.randint(1, 5) + _.randint(1, 10) // 10), el = _.randint(70, 500))
+#     person.save()
--- dhu_project/dhu_project/settings.py
+++ dhu_project/dhu_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&__&11ud(b^8!ge%9q+0oemcy=r0g-5b+=#o5!isky8g@16eq('
+SECRET_KEY = 'fjc27_*z*a^d@po^qvvj*uap5=ix@ifz6jjrnb0h!=3!3q7y&t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
