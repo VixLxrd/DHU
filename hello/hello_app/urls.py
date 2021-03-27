bfe1cedfa29996919fd0f38addbc0ac378dad7c0
max <komuxin06@mail.ru>
2021-03-27 17:19:23 +0500

    Создание в базе корректных данных (с 10001 по 10012)

--- hello/hello_app/urls.py
+++ hello/hello_app/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls import include, url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('anomalies/', views.anomalies)
+    path('anomalies/', views.anomalies),
+    path('', views.index)
 ]
